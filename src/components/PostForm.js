57f34b09a95f4de18e9d51967652aa129ac6c671
Kirillova <keira.kirillova@gmail.com>
2020-04-10 20:44:39 +0300

    add input change handler

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,11 +4,22 @@ export default class PostForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      postTitle: ''
+    }
   }
 
   submitHandler = (e) => {
     e.preventDefault()
+  };
+
+  inputChangeHandler = (e) => {
+    const name = e.target.name,
+      value = e.target.value;
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
   render() {
@@ -17,7 +28,14 @@ export default class PostForm extends React.Component {
         <form onSubmit={this.submitHandler}>
           <div className="form-group">
             <label htmlFor="title">Post name</label>
-            <input type="text" className="form-control" id="title" />
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              name="postTitle"
+              value={this.state.postTitle}
+              onChange={this.inputChangeHandler}
+            />
             <button type="submit" className="btn btn-success">Create post</button>
           </div>
         </form>
