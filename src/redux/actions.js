74bc37edfbe0825bd97817d04dd6a2c2330e5313
Kirillova <keira.kirillova@gmail.com>
2020-04-11 17:49:16 +0300

    add action fetchPosts

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,19 @@
-import {CREATE_POST} from "./types";
+import {CREATE_POST, FETCH_POSTS} from "./types";
 
 export function createPost(post) {
   return {
     type: CREATE_POST,
     payload: post
+  }
+}
+
+export function fetchPosts() {
+  return async dispatch => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+    const json = response.parse();
+    dispatch({
+      type: FETCH_POSTS,
+      payload: json
+    })
   }
 }
