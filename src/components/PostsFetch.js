fc84d8394a4e106aaf1a27c60bac0b025cc93907
Kirillova <keira.kirillova@gmail.com>
2020-04-11 18:19:23 +0300

    add component Loader

--- src/components/PostsFetch.js
+++ src/components/PostsFetch.js
@@ -2,21 +2,26 @@ import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchPosts} from "../redux/actions";
 import Post from "./Post";
+import {Loader} from "./Loader";
 
 export default () => {
   const dispatch = useDispatch();
   const posts = useSelector(state => state.posts.postsFetch);
+  const loading = useSelector(state => state.app.loading);
 
-  return (
-    <>
-      {posts.length ?
-        posts.map(post => <Post post={post} key={post.id}/>) :
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={() => dispatch(fetchPosts())}
-        >Загрузить</button>
-      }
-    </>
-  )
+  if (loading) {
+    return <Loader />
+  }
+
+  if (!posts.length) {
+    return (
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => dispatch(fetchPosts())}
+      >Загрузить</button>
+    )
+  }
+
+  return posts.map(post => <Post post={post} key={post.id}/>)
 }
